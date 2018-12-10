c9d7d499832be9f8e26a30047c8413df17054a9e
PierreBinet <pbinet@etud.insa-toulouse.fr>
2018-12-10 18:16:42 +0100

    ajout classe message

---
+++ ChatJava/src/Message.java
@@ -0,0 +1,21 @@
+import java.net.InetAddress;
+
+public class Message {
+	public void tryNickname() {}
+
+	public void isConnected() {}
+
+	public void newConnection() {}
+
+	public void connect() {}
+
+	public void disconnection() {}
+	
+	public void changedNickname() {}
+
+	public void discardOldNickname() {}
+
+	public void sendMdg() {}
+
+	public void sendFile() {}
+}
