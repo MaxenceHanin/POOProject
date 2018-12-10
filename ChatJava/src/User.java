4d3faa1e5cd49345f3d8883357e722a028260eda
MaxenceHanin <hanin@etud.insa-toulouse.fr>
2018-12-10 18:18:00 +0100

    User.java

--- ChatJava/src/User.java
+++ ChatJava/src/User.java
@@ -1,9 +1,32 @@
 /*import a voir*/
 import java.net.InetAddress;
 public class User {
-	public String nickname;
+	private String nickname;
 	public InetAddress addrUser;
 	private String pswd;
 	public boolean registered;
 	
+}
+
+public int login(String pwd, String username){
+	if ((pwd==this.pswd)&&(username==this.nickname)){
+		return 0;
+	}
+	return -1;
+}
+
+public void logout(String pwd, String username){
+}
+
+public void register(){
+}
+
+public void changeNickname(String newname){
+	if(Agent.checkUnicityNickname()){
+		this.nickname = newname;
+		controller.UpdateListUsedNicknames();
+	}
+}
+public String getNickname(){
+	return this.nickname;
 }
