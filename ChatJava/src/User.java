bf35d8aadb4296775271fdd5f797f36764a24b09
MaxenceHanin <hanin@etud.insa-toulouse.fr>
2018-12-10 17:49:46 +0100

    first commit

---
+++ ChatJava/src/User.java
@@ -0,0 +1,9 @@
+/*import a voir*/
+import java.net.InetAddress;
+public class User {
+	public String nickname;
+	public InetAddress addrUser;
+	private String pswd;
+	public boolean registered;
+	
+}
