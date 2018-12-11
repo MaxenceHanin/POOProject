4ec3d6253ac9425fa4a4768d3eaa72495cfa0ed5
PierreBinet <pbinet@etud.insa-toulouse.fr>
2018-12-11 16:45:40 +0100

    ajout class agent

---
+++ ChatJava/src/Agent.java
@@ -0,0 +1,18 @@
+/*import a voir*/
+import java.net.InetAddress;
+import java.util.List;
+import java.util.LinkedList;
+
+public class Agent {
+	
+	public List listChatOpened = new LinkedList();
+	
+	public void checkUnicityNickname() {
+		
+	}
+	
+	public void chooseFile() {
+		
+	}
+	
+}
