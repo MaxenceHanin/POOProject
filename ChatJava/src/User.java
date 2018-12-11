98c68c55d443b2e68308d690f07d924023aefd5f
MaxenceHanin <hanin@etud.insa-toulouse.fr>
2018-12-11 16:21:44 +0100

    user

--- ChatJava/src/User.java
+++ ChatJava/src/User.java
@@ -5,28 +5,34 @@ public class User {
 	public InetAddress addrUser;
 	private String pswd;
 	public boolean registered;
-	
-}
 
 public int login(String pwd, String username){
+	
 	if ((pwd==this.pswd)&&(username==this.nickname)){
+		controller.updateListConnectedUser(nickname,1);
 		return 0;
 	}
 	return -1;
 }
 
 public void logout(String pwd, String username){
+	controller.updateListConnectedUser(nickname,0);
 }
 
-public void register(){
+public void register(String username){
+	while (not registered) loop{
+		
+	}
 }
 
 public void changeNickname(String newname){
 	if(Agent.checkUnicityNickname()){
 		this.nickname = newname;
-		controller.UpdateListUsedNicknames();
+		controller.updateListUsedNicknames();
 	}
 }
 public String getNickname(){
 	return this.nickname;
+}
+
 }
