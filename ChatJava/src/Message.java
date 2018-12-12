e4d0bdeb2497725e5b029a136e2a205f645e0e37
PierreBinet <pbinet@etud.insa-toulouse.fr>
2018-12-12 08:16:57 +0100

    completion du message de new connection

--- ChatJava/src/Message.java
+++ ChatJava/src/Message.java
@@ -7,24 +7,41 @@ public class Message {
 	//payload 
 	//
 	
-	private static String FLAG_SP = "1";
 	private static String FLAG_NORM = "0";
+	private static String FLAG_SPE = "1";
 	
 	private static String NEW_CO = "0";
 	private static String CONNECT = "1";
+	private static String DISCONNECT = "2";
+	private static String TRY_NKNM = "3";
+	private static String CHGD_NKMN = "4";
+
+	private static String SEND_MSG = "0";
+	private static String SEND_FILE = "1";
 	
-	public void buildPayload
+	
 	
 	public void newConnection(User user) {
 		String payload = new String();
-		payload.concat(FLAG_SP);
-		payload.concat()
+		payload.concat(FLAG_SPE);
+		payload.concat(NEW_CO);
+		payload.concat(user.getNickname());
+		
 		//la payload doit contenir le flag special mis sur true (1),
 		//le nickname de l'utilisateur envoyant le message de nouvelle connection
 		SysCom.send(user.getAddr(),Agent.getAddrLAN(),payload);
 	}
 
-	public void connect() {}
+	public void connect() {
+		String payload = new String();
+		payload.concat(FLAG_SPE);
+		payload.concat(NEW_CO);
+		payload.concat(user.getNickname());
+		
+		//la payload doit contenir le flag special mis sur true (1),
+		//le nickname de l'utilisateur envoyant le message de nouvelle connection
+		SysCom.send(user.getAddr(),Agent.getAddrLAN(),payload);
+	}
 	
 	public void tryNickname() {}
 
