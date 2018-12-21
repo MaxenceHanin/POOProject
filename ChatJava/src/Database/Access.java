b41002d092f6291e581093b7c2c831b52b0ef1d8
MaxenceHanin <hanin@etud.insa-toulouse.fr>
2018-12-21 12:10:26 +0100

    debut implementation datatbase

--- ChatJava/src/Database/Access.java
+++ ChatJava/src/Database/Access.java
@@ -1,13 +1,22 @@
 package Database;
-import java.sql
-import java.sql.DriverManager;*;
+import java.sql.*;
 
 public class Access {
-String dataBaseURL = "jdbc:mysql://localhost:3306/demo";
-String user = "...";
-String pass = "...";
-
-Connection myConn = DriverManager.getConnection(dataBaseURL,user,pass);
-
+	public void AccessDB() {
+		String dataBaseURL = "jdbc:mysql://localhost:3306/demo"; //default address
+		String user = "...";
+		String pass = "...";
+		try {
+			Connection myConn = DriverManager.getConnection(dataBaseURL,user,pass);
+			
+			Statement myStmt = myConn.createStatement();
+			
+			ResultSet myRs = myStmt.executeQuery();
+		} catch(java.sql.SQLException e) {
+			System.out.println("mauvaise URL pour la DB ou mauvais identifiants pour y acceder");
+		}
+		
+		
+	}
 
 }
