7a945ac579babdb839658ee895a1d18f38d304e3
Dharsy <dharsy@outlook.fr>
2019-01-17 10:58:41 +0100

    Client gere le login et register

--- ChatJava/src/display/RegWindow.java
+++ ChatJava/src/display/RegWindow.java
@@ -1,40 +1,34 @@
 package display;
-import java.io.IOException;
-
-import database.Access;
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import client.Client;
+import database.Access;
 
 public class RegWindow extends Parent {
 	/*private int btnWidth = 0;*/
 	static GridPane grid3 = new GridPane();
+	String currentLogin;
 	
 	public void setBtn(Button btn, String txt){
         btn.setText(txt);
         btn.setMinWidth(80);
 	}
 
-    public RegWindow() {
+    public RegWindow(Client client, Access BDD) {
     	
     	 
-        TextField txtLog = new TextField("Entrez l'identifiant souhaité");
+        TextField txtLog = new TextField("Entrez l'identifiant souhaitï¿½");
         txtLog.setMinWidth((int)(0.6*DisplayLogin.X));
     	Button btn = new Button();
     	setBtn(btn,"Inscription");
@@ -42,10 +36,9 @@ public class RegWindow extends Parent {
     	
         btn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
-                /*test success login*/
             	String Log = String.valueOf(txtLog.getCharacters());
-            	if (!LoginWindow.BDD.userExists(Log)) {
-            		LoginWindow.BDD.setUserConnected(Log);
+            	if(client.register(Log, BDD)) {
+            		currentLogin = Log;
                 	StackPane rootChat = new StackPane();
                     ChatWindow chatwindow = new ChatWindow();
                     rootChat.getChildren().add(chatwindow);
@@ -63,9 +56,8 @@ public class RegWindow extends Parent {
                     ((Node)(event.getSource())).getScene().getWindow().hide();
                 }
             	else {
-            		Label errLog = new Label("L'enregistrement a échoué pour " +"'" + Log + "'"+": "+"pseudo déjà utilisé");
+            		Label errLog = new Label("L'enregistrement a ï¿½chouï¿½ pour " +"'" + Log + "'"+": "+"pseudo dï¿½jï¿½ utilisï¿½");
             	    grid2.add(errLog, 0, 4);
-            		//errLog.setVisible(true);
                 }
             }
         });
