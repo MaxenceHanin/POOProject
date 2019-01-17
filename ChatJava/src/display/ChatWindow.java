2839b0c96f1697c6bfddb0e85ebea02a677e1591
Maxence <mhanin@hotmail.fr>
2019-01-17 10:16:26 +0100

    affichage des msg d'une conv

--- ChatJava/src/display/ChatWindow.java
+++ ChatJava/src/display/ChatWindow.java
@@ -1,18 +1,26 @@
 package display;
 
+import java.sql.ResultSet;
+import java.util.HashSet;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 public class ChatWindow extends Parent {
 	static String ActualConv = "on verra";
@@ -34,12 +42,14 @@ public class ChatWindow extends Parent {
         btntxt.setHgap(10);
         btntxt.add(txt,0,0);
         btntxt.add(btn,1,0);
-        ScrollPane convscroll = new ScrollPane();
+        FlowPane convscroll = new FlowPane();        
+        
         GridPane conv = new GridPane();
         conv.setMinHeight(200);
         Background backConv = new Background(new BackgroundFill(Color.BEIGE, new CornerRadii(2), new Insets(2)));
         conv.setBackground(backConv);
         convscroll.setPrefSize(conv.getPrefWidth(),conv.getPrefHeight()); 
+
         conv.add(convscroll, 0, 0);
 		
 		GridPane grid4 = new GridPane();
@@ -55,16 +65,24 @@ public class ChatWindow extends Parent {
 		grid4.setVgap(15);
 		grid4.add(btntxt,2,1);
 		grid4.add(conv,2,0);
-		
+		// a un moment il faut afficher quelque part le nom de la conv affichée sur la grid
+		Label ConvOuverte = new Label(ActualConv);
 //---------------------------------------------------------
 		Button UseLog = new Button("Patou");
     	UseLog.setText("Patou");
         UseLog.setMinWidth(UserLogged.getPrefWidth());
-        
         UseLog.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
+
                 /*affichage de la conversation entre user actuel et user cliqué*/
-            	LoginWindow.BDD.extractMsg(databaseAlreadyExists(UseLog.getContentDisplay(),LoginWindow.ActualLogin));
+            	String ActualConv = LoginWindow.BDD.databaseAlreadyExists(currentDestUser,LoginWindow.ActualLogin);
+            	ResultSet myRs = LoginWindow.BDD.extractMsg(ActualConv);
+            	//System.out.println(myRs.getString("snick")+" -> "+myRs.getString("dnick")+" @"+myRs.getTime("time")+" : "+myRs.getString("text"));
+            	while (myRs.next()) {
+            		convscroll.getChildren().add(new Text(myRs.getString("text")+"( @"+myRs.getTime("time")+")"));
+            		convscroll.getChildren().add(new Text("           ***          "));
+            		//ConvSet.add(new Text(myRs.getString("text")+"( @"+myRs.getTime("time")+")"));
+    			}
             }
         });
 
