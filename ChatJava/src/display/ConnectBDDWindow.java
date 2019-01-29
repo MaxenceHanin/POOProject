58fc839c5aa0219a5d203c92abfcc9132fe70060
Maxence <mhanin@hotmail.fr>
2019-01-29 14:57:34 +0100

    ajout de la fenetre pour la creation de la BDD

---
+++ ChatJava/src/display/ConnectBDDWindow.java
@@ -0,0 +1,69 @@
+package display;
+import database.Access;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import client.*;
+
+public class ConnectBDDWindow extends Parent {
+	/*private int btnWidth = 0;*/
+	static GridPane grid3 = new GridPane();
+	static String currentLogin;
+	static Access BDD;
+	
+	public void setBtn(Button btn, String txt){
+        btn.setText(txt);
+        btn.setMinWidth(80);
+	}
+
+    public ConnectBDDWindow() {
+    	
+        TextField txtLog = new TextField("Entrez l'identifiant de la base de donnée");
+        PasswordField pswd = new PasswordField();
+        txtLog.setMinWidth((int)(0.6*DisplayLogin.X));
+        pswd.setMinWidth((int)(0.6*DisplayLogin.X));
+
+    	Button btn = new Button();
+    	setBtn(btn,"login");
+    	GridPane grid2 = new GridPane();
+    	
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {             
+            	String Log = String.valueOf(txtLog.getCharacters());
+            	String pwd = String.valueOf(pswd.getCharacters());
+            		try {
+            			BDD = new Access(Log,pwd);
+            		} catch(SQLException e) {
+            			Label err = new Label(e.getLocalizedMessage());
+            			grid2.add(err, 1, 1);
+            		}
+            }
+        });
+        
+        //grid2.prefWidthProperty().bind(DisplayLogin.root.widthProperty());
+        //grid2.prefHeightProperty().bind(DisplayLogin.root.heightProperty());
+        grid2.setPadding(new Insets(20));
+        grid2.setHgap(25);
+        grid2.setVgap(15);
+        grid2.add(txtLog,0,1);
+        grid2.add(pswd,0,2);
+        grid2.add(btn, 0, 3);
+        this.getChildren().add(grid2);
+    }
+
+}
